f55ac2f92115956ca07d117861f1cf896e599a94
pid93 <woda634@gmail.com>
2026-01-13 22:45:50 +0100

    cleanup_nvim

--- nvim/lua/plugins/telescope.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "nvim-telescope/telescope.nvim",
-  keys = {
-    { "<leader><space>", "<cmd>Telescope git_files<cr>", desc = "Find Files (git-files)" },
-  },
-}
--- nvim/lua/plugins/tmux-navigation.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "alexghergh/nvim-tmux-navigation",
-  config = function()
-    local nvim_tmux_nav = require("nvim-tmux-navigation")
-    nvim_tmux_nav.setup({
-      disable_when_zoomed = true,
-    })
-  end,
-}
