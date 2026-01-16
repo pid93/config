43171f08c6741eb857dc63c5bf3d30d20f59c993
pid93 <woda634@gmail.com>
2026-01-16 17:57:27 +0100

    typescript prettier

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -11,6 +11,7 @@ return {
 				rust = { "rustfmt", lsp_format = "fallback" },
 				-- Conform will run the first available formatter
 				javascript = { "prettierd", "prettier", stop_after_first = true },
+				typescript = { "prettierd", "prettier", stop_after_first = true },
 			},
 		})
 		vim.api.nvim_create_autocmd("BufWritePre", {
--- nvim/lua/plugins/mason-lspconfig.lua
+++ nvim/lua/plugins/mason-lspconfig.lua
@@ -1,7 +1,7 @@
 return {
 	"mason-org/mason-lspconfig.nvim",
 	opts = {
-		ensure_installed = { "lua_ls", "rust_analyzer", "angularls", "vtsls", "stylua", "prettier", "prettierd" },
+		ensure_installed = { "lua_ls", "rust_analyzer", "angularls", "vtsls", "stylua" },
 	},
 	dependencies = {
 		{ "mason-org/mason.nvim", opts = {} },
